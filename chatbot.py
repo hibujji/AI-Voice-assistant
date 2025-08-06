9df3999dccbfd1969b5daa92b0f1ada3baea3734
hibujji <muralyjagarapu2002@gmail.com>
2025-08-06 15:30:17 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1w4oLOxrnFmA5niVpC-b-7V_TmsJjVwuX
 """
 
-!pip install streamlit
+""" !pip install streamlit """
 
 import streamlit as st
 
